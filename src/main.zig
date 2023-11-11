88d889d8de6119c0c7c3b25cbc24f6bb2e6432a8
Vano <sogwyms@gmail.com>
2023-11-11 18:50:23 +0800

    test cli and csm

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,59 @@
+const std = @import("std");
+
+const mcli = @import("cli/mcli.zig");
+
 const uart = @import("uart.zig");
 const putc = uart.putc;
 const getc = uart.getc;
 const puts = uart.puts;
+const csm = @import("csm.zig");
 
-const std = @import("std");
+fn csm_cb_complete(s: []const u8) void {
+    puts("\r\n");
+    puts(s);
+    puts("\r\nmcli:~$ ");
+}
 
 pub fn main() void {
     puts("Hello World\r\n");
 
+    var cli = mcli.new();
+
+    var _reader = mcli.sreader_from_buffer("echo hello\r\n");
+
+    var _writer = mcli.swriter_from_putc_without_ctx(putc);
+
+    cli.call(_reader.reader(), _writer.writer(), _writer.writer());
+
+    puts("\r\nmcli:~$ ");
+
+    var mcsm = csm.new(puts, csm_cb_complete);
     while (true) {
-        putc('.');
+        // putc('.');
         delay();
         if (getc()) |d| {
-            putc(d);
+            // if (d == 'q') {
+            //     break;
+            // } else if (d == ascii.DEL) {
+            //     // puts("\r\ndel\r\n");
+            //     putc(ascii.BS);
+            //     putc(' ');
+            //     putc(ascii.BS);
+            // } else if (d == ascii.ESC) {
+            //     putc(ascii.CR);
+            // }
+            // putc(d);
+
+            // putc_in_hex(d);
+
+            mcsm.inputc(d);
         }
     }
 }
 
 fn delay() void {
     var i: u32 = 0;
-    while (i < 10000000) {
+    while (i < 100000) {
         i += 1;
     }
 }
